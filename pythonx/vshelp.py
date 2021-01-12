idbrii/vim-vshelp

commit 3fa9b9ea59018400766cc0df2239ab9a282d7b08

Disable broken line jumping for fallback
Edit.Goto doesn't work from command line (doesn't move cursor) and
causes error from python (it thinks 3 is a file). Disable it.

Also cleanup this code.

pythonx/vshelp.py
@@ -36,9 +36,11 @@ def _find_latest_devenv():
 
 def _open_with_batch(filename, line):
     cmd = [os.path.join(_find_latest_devenv(), "devenv")]
-    cmd.extend(['/Edit "{filename}"'.format(filename=filename),
-            '/Command "Edit.GoTo {line}"'.format(line=line)])
-    ret = subprocess.call(cmd)
+    cmd.extend([
+        f'/Edit {filename}',
+        # Edit.Goto always results in a new VS instance and an error.
+        # f'/Command Edit.GoTo {line}',
+    ])
     if ret != 0:
         print("devenv.exe invocation failed: {code}".format(code=ret))
 

commit e0b3f0f94c59bd976f06d097c495c44cfd49142b

Restore accidental removal of calling the subprocess
3fa9b9e accidentally removed this line which breaks this fallback.

Test
Fallback doesn't work for VS2017 -- VS gives an error, fails to open the
file, and doesn't launch a subprocess so vim blocks until VS is closed.

pythonx/vshelp.py
@@ -41,6 +41,7 @@ def _open_with_batch(filename, line):
         # Edit.Goto always results in a new VS instance and an error.
         # f'/Command Edit.GoTo {line}',
     ])
+    ret = subprocess.call(cmd)
     if ret != 0:
         print("devenv.exe invocation failed: {code}".format(code=ret))
 